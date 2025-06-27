a213a87446ba059f66a77846ecf3cf984448f0ae
Patrik Gugh <gugh.patrik@gmail.com>
2025-06-27 13:17:47 +0200

    Add MainPage

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,10 @@
+export default function MainPage() {
+  return (
+    <>
+      <div className="text-center">
+        <i className="pi pi-amazon"></i>
+        <p>this is main page</p>
+      </div>
+    </>
+  )
+}
